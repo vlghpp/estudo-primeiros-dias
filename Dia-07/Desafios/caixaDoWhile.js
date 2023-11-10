cdb1230a31ead00e1eeb8a1a91e8fa4b7c05e16e
Henrique Patricio <henriquepworking@gmail.com>
2023-11-10 16:00:54 -0300

    adicionando conteúdo sobre while e do while

--- Dia-07/Desafios/caixaDoWhile.js
+++ Dia-07/Desafios/caixaDoWhile.js
@@ -1,3 +1,28 @@
+/*
+2 - Caixa eletrônico (DOWHILE)
+
+*Não esqueça de testar seu código!
+
+-Solicite o nome e o cpf do seu cliente.
+-Pergunte um valor.
+-Ofereça a opção de saque ou depósito.
+-Caso a opção de saque seja a escolhida, remova o valor inserido do saldo total.
+-Caso a opção de depósito seja escolhida, adicione o valor inserido do saldo total.
+-Pergunte se o seu usuário deseja continuar. (Insira 1 para continuar e 2 para parar).
+-Exiba o saldo total.
+-Exiba o maior valor inserido(independente de ser depósito ou saque).
+-Exiba a média dos valores inseridos(independente de ser depósito ou saque).
+
+REGRAS: 
+    1- Considere iniciar a variável saldo com algum valor
+
+    1- Não deve ser permitido sacar um valor não disponível no saldo.
+
+    2- Não deve ser permitido sacar nem depositar um valor negativo.
+
+ */
+
+
 let nome = prompt("Qual é seu nome ? ");
 let cpf = prompt("Qual é seu cpf ? ");
 let valor = Number(prompt("Quanto você tem? "));
--- Dia-07/Desafios/escolaWhile.js
+++ Dia-07/Desafios/escolaWhile.js
@@ -1,3 +1,18 @@
+/*
+1 - School (WHILE)
+
+Desenvolva um algoritmo que receba a nota e o sexo do aluno, usando a estrutura WHILE, e pergunte ao usuario se ele deseja cadastrar outra nota. No final, exiba no console:
+
+
+- A média geral dos alunos
+- Quantos homens enviaram as notas
+- Quantas mulheres tiveram nota acima de 7
+- Qual a maior nota entre os homens
+
+*Obs.: para obter a média geral deve somar todas as notas e dividir pelo número de alunos! */
+
+
+
 let contadorM = 0;
 let contadorF = 0;
 let contadorF7 = 0;
